eb9d7ef496b9de750d28a7fc5a1d3a1aef582e8b
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-08 14:19:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function  daysOfAYear(input) {
+	if(input % 4 == 0 && input % 100 != 0) || (input% 400 == 0)
+	{
+		return 366;
+	}
+	else
+	{
+		return 365;
+	}
+}
+let input=prompt("enter input");
+alert(daysOfAYear(input));
